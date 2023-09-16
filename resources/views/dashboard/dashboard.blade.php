7abe4ab4948247a87d05cbc03be678788398704e
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-09-16 15:32:21 +0200

    stile visibilità categorie

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -9,19 +9,22 @@
                 <div class="card">
                     <img src="{{ asset('storage/images/' . $category->img) }}" class="card-img-top"
                         alt="{{ $category->name }}">
-                    <div class="card-body text-center"> <!-- Aggiunto text-center qui -->
+                    <div class="card-body text-center">
                         <h5 class="card-title">{{ $category->name }}</h5>
-                        <a href="{{ route('category.show', $category->id) }}" class="btn btn-primary">Vai a
+                        <a href="{{ route('category.show', $category->id) }}" class="btn btn-primary mb-2">Vai a
                             {{ $category->name }}</a>
-                        <div class="buttons-container">
-                            <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-primary">Modifica
+
+                        <div class="d-flex justify-content-between align-items-center mt-3">
+                            <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-warning">Modifica
                                 Categoria</a>
+
                             <form action="{{ route('categories.toggleVisibility', $category->id) }}" method="post">
                                 @csrf
-                                <div class="form-group">
-                                    <label for="is_hidden_{{ $category->id }}">Nascondi Categoria</label>
-                                    <input type="checkbox" id="is_hidden_{{ $category->id }}" name="is_hidden"
-                                        {{ $category->is_hidden ? 'checked' : '' }} onchange="this.form.submit()">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="is_hidden_{{ $category->id }}"
+                                        name="is_hidden" {{ $category->is_hidden ? 'checked' : '' }}
+                                        onchange="this.form.submit()">
+                                    <label class="form-check-label" for="is_hidden_{{ $category->id }}">Nascondi</label>
                                 </div>
                             </form>
                         </div>
